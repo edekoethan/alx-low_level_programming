f92b7582fd12bbd915b8c63e2eca403481acd962
edeko ethan <eromoseleeeko99@gmail.com>
2023-04-05 15:08:58 -0700

    update-scripts-on-0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,26 +1,20 @@
-/*
- * File: 8-sum_listint.c
- * Auth: Ethan Edeko
- */
-
 #include "lists.h"
 
 /**
- * sum_listint - Calculates the sum of all the
- *               data (n) of a listint_t list.
- * @head: A pointer to the head of the listint_t list.
- *
- * Return: If the list is empty - 0.
- *         Otherwise - the sum of all the data.
+ * sum_listint - calculates the sum of all the data in a listint_t list
+ * @head: first node in the linked list
+ * author edeko ethan
+ * Return: resulting sum
  */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
+	listint_t *temp = head;
 
-	while (head)
+	while (temp)
 	{
-		sum += head->n;
-		head = head->next;
+		sum += temp->n;
+		temp = temp->next;
 	}
 
 	return (sum);
