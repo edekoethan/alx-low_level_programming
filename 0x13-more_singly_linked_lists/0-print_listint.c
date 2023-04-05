9fb20b7121d066f8b78b15296fa800ef8d1d0df9
edeko ethan <eromoseleeeko99@gmail.com>
2023-04-05 14:35:33 -0700

    my-01-update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,27 +1,21 @@
-/*
- * File: 0-print_listint.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @v: A pointer to the head of the list_t list.
- *
- * Return: The number of nodes in the list_t list.
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ * author : edeko ethan
+ * Return: number of nodes
  */
-size_t print_listint(const listint_t *v)
+size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t num = 0;
 
-	while (v)
+	while (h)
 	{
-		nodes++;
-		printf("%d\n", v->n);
-		v = v->next;
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
 	}
 
-	return (nodes);
+	return (num);
 }
