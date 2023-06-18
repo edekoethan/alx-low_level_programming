7051002395c7dae6348f03576b36d862aebc1c8c
edekoethan <124743075+edekoethan@users.noreply.github.com>
2023-06-18 07:47:08 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,14 +17,14 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
+	dlistint_t *tmp = *h, *news;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	for (; idx != 1; idx--)
 	{
-		tmp = tmp->next;
+		
 		if (tmp == NULL)
 			return (NULL);
 	}
